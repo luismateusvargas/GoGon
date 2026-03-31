e4b33a1bfe2d1c70d2f7cfac64f8ececb45fa7a2
ColonelReaper <69818305+ColonelReaper@users.noreply.github.com>
2026-03-31 09:49:57 -0300

    Refactor webhooks and group constants
    Removed hardcoded webhook URLs and group IDs, replacing them with empty strings for future configuration.

--- webhooks.js
+++ webhooks.js
@@ -1,49 +1,23 @@
-// RIOT WEBHOOKS
-export const relicWebhook = 'https://discord.com/api/webhooks/1402982809165107230/bzNPsfHFP3_uPNnr8uEp2zcDkbaRuBDNe_sLeVTmyLsFtHTwv5QKMaC55gDOoKLT_LEg';
-export const conflictWebhook = 'https://discord.com/api/webhooks/1402983130045878292/DW6WOyX8aUgkfpx2-j01Pjh-ayqw49oUMO8EMsvZxOO9fwArEzRnfcXhaxx37hN9wbfq';
-export const bountyWebhook = 'https://discord.com/api/webhooks/1111675211968950363/xwT5Ju0nkGSRfTkaUwl_AkNTKry5kAuUYyxLuvDmfGbemT5gdjLrdJ49piOA4wluvBO0';
-export const titanWebhook = 'https://discord.com/api/webhooks/1111675919829045289/Y6WgHBE8imPTK8eLuoZNyYxz5JhuNS4uAStV2DiEIWuvfRlDUP6gg_le0CAL-yXVgoeH';
-export const ladderWebhook = 'https://discord.com/api/webhooks/1111676083587260418/d_DD4FuHLW-TXSUiqxmV00zYxrd7JlNO_SlF39YeA_52gRonS58TVC-aoCoduT_3xY2h';
-export const ladderRankingWebhook = 'https://discord.com/api/webhooks/1402422545696821298/vnC00pqrRzdlUZ5IuOzOPMt9_iWuDcHImUsZ-XgkUak5OSYagpWM8ILw5iubOgMy9P3g';
-export const SuperEliteWebhook = 'https://discord.com/api/webhooks/1125465684906868886/RXLdFdW0ZGKH7c9Ku8u7-oiiTRC4RHpUWcAUUs-bMtHoDMzqyKzHXow133ymEXytOwXx';
-export const cratesWebhook = 'https://discord.com/api/webhooks/1142943755259875389/pxspjNuDAWD-xJNJPxX4KhYZMHmC2S_Lxm5koGDQh3wjfpR792BBfBw3eTyGSXxRfYfa';
-export const newsWebhook = 'https://discord.com/api/webhooks/1142972994478686279/16H0SeTON7va5l6REkzpnTs-jpkebT-rqIJbXYEKV1Bt_J-HiQHeSH7KY66DIRAhtB6L';
-export const shoutboxWebhook = 'https://discord.com/api/webhooks/1142973656302092358/qp-pFvUFWp9ErLl9PW8h3sktFREZUn2PvdqHNbluUEVHCPVsXBjhF87Wn_-DWhU54PbP';
-export const guildMessageWebhook = 'https://discord.com/api/webhooks/1417198719501209800/LpCujw9I5IEdJUZQcXAlT-oleZywFNqx5tOgVoouYfnhUDIATVXKOlonq-_vJETwrdfm';
+// WEBHOOKS
+export const relicWebhook = '';
+export const conflictWebhook = '';
+export const bountyWebhook = '';
+export const titanWebhook = '';
+export const ladderWebhook = '';
+export const ladderRankingWebhook = '';
+export const SuperEliteWebhook = '';
+export const cratesWebhook = '';
+export const newsWebhook = '';
+export const shoutboxWebhook = '';
+export const guildMessageWebhook = '';
 
-/*
-// TEST WEBHOOKS 
-export const relicWebhook = 'https://discord.com/api/webhooks/1411386166980251790/fLby81B96SxcMKfUgvU1VOAPVdyCr3g3d_yCQ_RgN4J3easgHlegCvo1xp9TQRD9ovV8';
-export const conflictWebhook = 'https://discord.com/api/webhooks/1411386229660061748/AWCSiNs9JeScKdwl_xAlUoU1ue4PbmEHD5W2GItT2d4cX72gjM2TaLCcQz0R_zSp6H98';
-export const bountyWebhook = 'https://discord.com/api/webhooks/1103496248557838336/5Q0TOaVoeHeYQtCkq9m3nbcVCm-slD-Z_9xFGJangFSs1BMWl1qlSSAsPwA_h_Np5PYJ';
-export const titanWebhook = 'https://discord.com/api/webhooks/1103662228797931621/RKfIWrsKC61p8ZCFClT49AtWFuoD3l6x4ph-36bNG8ZAdMT3SpeovDrw_M4UWZiUVsFo';
-export const ladderWebhook = 'https://discord.com/api/webhooks/1103663332222832682/MsCU48Y6e9uKA9AQH-ht3lNjn-zpQjv8Ah_x5LKkASEexbaZddapp9X3actcUvp7-67W';
-export const ladderRankingWebhook = 'https://discord.com/api/webhooks/1402422545696821298/vnC00pqrRzdlUZ5IuOzOPMt9_iWuDcHImUsZ-XgkUak5OSYagpWM8ILw5iubOgMy9P3g';
-export const SuperEliteWebhook = 'https://discord.com/api/webhooks/1142883657195995146/gIFWw1Ez7pbO-VT5H_RIA8lnLoCjRR72xh5kKsTyeT8SfRlKCaB-3QCfTNQ6PoqZR6h9';
-export const cratesWebhook = 'https://discord.com/api/webhooks/1142942202515955873/VPD51apLaU2rUecXRvTl8aCa-s4Ns3Lc7PCsje_VDlDVU2JJiiUBuuiDeGK5PfJDswhb';
-export const newsWebhook = 'https://discord.com/api/webhooks/1142952377767440424/Uq0hWKi1FpW5UgtqCYALaTTILAiLq83mqImbOcLC4EN8na7obGccwUgOOr1ZKMgQqct6';
-export const shoutboxWebhook = 'https://discord.com/api/webhooks/1142967566185279499/7pnwNPxYG8OiqH-L7H0o6ScEMHdQiEOBesS9iS2I6961RJaugo05SzjyAdjhL_v7lsHs';
-export const guildMessageWebhook = 'https://discord.com/api/webhooks/1421193732031643739/i9Jyuuc0S_sUzpxPjjyYMTCfA3VUMi8endxxulTUylfdHjY9h5APQVuyxb2tCEJ0YMT0'
-*/
+// GROUPS
+export const SEgroup = '<@&>';
+export const BountyGroup = '<@&>';
+export const TitanGroup = '<@&>';
+export const LadderGroup = '<@&>';
+export const EveryoneGroup = '<@>';
+export const RioterGroup = '<@>';
 
-// RIOT GROUPS
-export const SEgroup = '<@&1142926613009408000>';
-export const BountyGroup = '<@&1111676687340548216>';
-export const TitanGroup = '<@&1111676811982667776>';
-export const LadderGroup = '<@&1111676962067456091>';
-export const EveryoneGroup = '<@880193874662916166>';
-export const RioterGroup = '<@880276104626180176>';
-
-/*
-//TEST GROUPS
-export const SEgroup = '<@&1142916721276104744>';
-export const BountyGroup = '<@&1103491749743185930>';
-export const TitanGroup = '<@&1103705296276304032>';
-export const LadderGroup = '<@&1103705432842846218>';
-*/
 export const DELAY_BETWEEN_MESSAGES = 2000;
 export const RETRY_DELAY = 2000;
-
-
-//BOT CONFIG ID IS 1089961388480405525
-//RIOT ID IS 880193874662916166
